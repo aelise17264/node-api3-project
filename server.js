f7f17e7fcb6c4b511774dc072bf7e56996591118
unknown <aelise17264@gmail.com>
2020-09-30 15:27:46 -0600

    updated logger now console.log showing up

--- server.js
+++ server.js
@@ -5,23 +5,24 @@ const server = express();
 
 server.use(express.json())
 
-server.use('/api/users', userRouter)
-server.use('/api/posts', postRouter)
+
 
 
 //custom middleware
 
 function logger(req, res, next) {
-  console.log(  `[${new Date().toISOString()}] ${req.method} to ${req.url} from 	${req.get('Origin')}`	  );
- res.status(200).json();
-  next();		}
+  console.log(  `[${new Date().toISOString()}] ${req.method} to ${req.url} from ${req.get('host')}`	  );
+ //res.status(200).json();
+ next();
+		}
 
 
 server.use(logger)
+server.use('/api/users', userRouter)
+server.use('/api/posts', postRouter)
 
 
-
-server.get('/', (req, res) => {
+server.get('/api/users', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
