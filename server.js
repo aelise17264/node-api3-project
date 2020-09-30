d2907d7ab0e9a8a563fe679dc3a7c74d6a64b7a9
unknown <aelise17264@gmail.com>
2020-09-30 13:13:57 -0600

    posts and gets added to router

--- server.js
+++ server.js
@@ -1,19 +1,27 @@
 const express = require('express');
-
+const userRouter = require('./users/userRouter')
 const server = express();
+
+server.use(express.json())
+
+server.use('/', userRouter)
 
 //custom middleware
 
 function logger(req, res, next) {
-  console.log(  `[${new Date().toISOString()}] ${req.method} to ${req.url} from 	${req.get('Origin')}`	  );
+  //console.log(  `[${new Date().toISOString()}] ${req.method} to ${req.url} from 	${req.get('Origin')}`	  );
+ res.status(200).json();
   next();		}
 
 
 server.use(logger)
 
+
+
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
+
 
 
 module.exports = server;
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const Posts = require('./userDb')
 
 const router = express.Router();
 
 router.post('/', (req, res) => {
   // do your magic!
+  Posts.insert(req.body)
+  .then(user => {
+    res.status(201).json(user)
+  })
+  .catch(error => {
+    console.log(error)
+    res.status(500).json({message: 'Error adding user'})
+  })
+ 
 });
 
 router.post('/:id/posts', (req, res) => {
@@ -12,10 +22,30 @@ router.post('/:id/posts', (req, res) => {
 
 router.get('/', (req, res) => {
   // do your magic!
+  Posts.get()
+  .then(post => {
+      res.status(200).json(post)
+  })
+  .catch(error => {
+    console.log(error)
+    res.status(500).json({ message: 'Error retrieving users'})
+  })
 });
 
 router.get('/:id', (req, res) => {
   // do your magic!
+  Posts.getById(req.params.id)
+    .then(user => {
+      if(user){
+        res.status(200).json(user)
+      }else{
+        res.status(404).json({message: "User not found"})
+      }
+    })
+    .catch(error => {
+      console.log(error)
+      res.status(500).json({message: "Error adding user"})
+    })
 });
 
 router.get('/:id/posts', (req, res) => {
